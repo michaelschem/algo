fd5561ddf85e96c6e2c145a1ca6eaf39b4428a10
mschem <michaelfschem@gmail.com>
2022-01-17 23:13:48 -0500

    not working

--- flood.py
+++ flood.py
@@ -10,36 +10,47 @@ for i in range(len(grid)):
 		if grid[i][j] not in colors:
 			colors.append(grid[i][j])
 
-grid_vals = {}
-
 def print_grid(grid):
 	for line in grid:
 		print(line)
 
 print_grid(grid)
 
-def get_all_neighbors_count(color, i, j, visited=None):
+def get_neighbors(color, i, j):
 	offsets = [[1,0], [0,1], [-1,0], [0,-1]]
 
-	if visited == None:
-		visited = [[i, j]]
-	else:
-		visited.append([i, j])
-
+	neighbors = []
 	for offset in offsets:
 		try:
-			if grid[i+offset[0]][j+offset[1]] == color:
-				if [i+offset[0], j+offset[1]] not in visited:
-					visited = get_all_neighbors_count(color, i+offset[0], j+offset[1], visited)
+			if grid[i+offset[0]][j+offset[1]] == color:# and [i+offset[0], j+offset[1]] not in visited:
+					neighbors.append((color, i+offset[0], j+offset[1]))
 		except IndexError:
 			pass
 
-	return visited
+	return neighbors
+
+stack = []
+visited = {}
+max_len = 0
 
 for i,_ in enumerate(grid):
 	for j,color in enumerate(grid[i]):
-		if grid[i][j] == color:
-			grid_vals[i,j,color] = len(get_all_neighbors_count(color, i, j))
+		stack.append((grid[i][j],i,j))
 
-print(grid_vals)
+print(stack)
+while len(stack) > 0:
+	c,i,j = stack.pop()
 
+	neighbors = get_neighbors(c,i,j)
+
+	input()
+	print(visited)
+	print(stack)
+	if (i,j) not in visited:
+		visited[i,j] = len(neighbors)
+
+	unvisited_neighbors = [neighbor for neighbor in neighbors if (neighbor[0],neighbor[1]) not in visited]
+	stack.extend(unvisited_neighbors)
+
+print(visited)
+print(max_len)
