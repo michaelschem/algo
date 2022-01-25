5edb7aa0773bb5c7fb4e7202d74cd941aaf64c40
mschem <michaelfschem@gmail.com>
2022-01-24 22:18:27 -0800

    flood fix

--- flood.py
+++ flood.py
@@ -11,44 +11,61 @@ def print_grid(grid):
 print_grid(grid)
 
 class Node():
-	def __init__(self,c,i,j,visited):
-		self.c = c
+	def __init__(self,color,i,j):
+		print(color,i,j)
+		if type(i) != int and type(j) != int:
+			raise ValidationError()
+		self.color = color
 		self.i = i
 		self.j = j
-		visited[i,j] = None
-		self.visited = visited
 
 	@property
-	def unvisited_neighbors(self):
+	def neighbors(self):
 		offsets = [[1,0], [0,1], [-1,0], [0,-1]]
 
 		neighbors = []
 		for offset in offsets:
 			x,y = self.i+offset[0],self.j+offset[1]
 			try:
-				if grid[x][y] == self.c:
-					if (x, y) not in self.visited:
-						neighbors.append(Node(color, x, y,visited))
+				if grid[x][y] == self.color:
+					neighbors.append(Node(self.color, x, y))
 			except IndexError:
 				pass
 
 		return neighbors
+
+	def __repr__(self):
+		return f"<Node ({self.color}, {self.i}, {self.j})>"
+
+# node = Node('b',0,0)
+# print(node.neighbors)
 
 tree_lens = {}
 max_len = 0
 
 for i,_ in enumerate(grid):
 	for j,color in enumerate(grid[i]):
-		stack = []
+		stack = [Node(grid[i][j],i,j)]
 		visited = {}
-		stack.append(Node(grid[i][j],i,j,visited))
 
 		while len(stack) > 0:
 			node = stack.pop()
 
-			neighbors = node.unvisited_neighbors
+			input()
+			print("stack: ",stack)
+			print("visited: ",visited)
+			print("neighbors: ",node.neighbors)
+			print((node,j))
 
-			stack.extend(neighbors)
+			
+			visited[(node.i, node,j)] = None
+
+			# unvisited_neighbors = [neighbor for neighbor in node.neighbors if (neighbor.i,neighbor.j) not in visited]
+
+			for neighbor in node.neighbors:
+				if (neighbor.i, neighbor.j) not in visited:
+					stack.append(neighbor)
+
 
 		max_len = max(max_len, len(visited))
 		tree_lens[i,j] = len(visited)
